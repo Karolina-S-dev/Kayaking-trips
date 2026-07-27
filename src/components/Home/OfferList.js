28f47d03fe8535fefecae78d7f4cc93af147cd6c
Karo <karolina.szary93@gmail.com>
2026-07-27 22:07:27 +0200

    motion adjustments

--- src/components/Home/OfferList.js
+++ src/components/Home/OfferList.js
@@ -8,8 +8,8 @@ const OfferList = () => {
       <motion.section
         initial={{ opacity: 0, y: 24 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, amount: 0.2 }}
-        transition={{ duration: 0.8 }}
+        viewport={{ once: true, amount: 0.05 }}
+        transition={{ duration: 1 }}
         className="offer-section flex-col"
       >
         <p className="green offer-label">Oferta</p>
