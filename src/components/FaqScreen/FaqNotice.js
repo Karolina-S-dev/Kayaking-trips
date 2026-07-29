97cb30031a2b7235d76bfa689ef3c36a4798e1a9
Karo <karolina.szary93@gmail.com>
2026-07-29 10:42:11 +0200

    changed motion for faq notice

--- src/components/FaqScreen/FaqNotice.js
+++ src/components/FaqScreen/FaqNotice.js
@@ -7,7 +7,8 @@ const FaqNotice = () => {
     <>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.2 }}
         transition={{ duration: 0.6, ease: "easeOut" }}
         className="faq-notice flex-row"
       >
