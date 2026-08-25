3b8fad327e69a13ed966f3d4b19a8a6ca9c1621a
Karo <karolina.szary93@gmail.com>
2026-08-25 13:23:14 +0200

    React Hook Form

---
+++ src/components/ContactScreen/ContactForm.js
@@ -0,0 +1,206 @@
+import { useEffect, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import { reusableUpdateField } from "../../utils/functions";
+import InputPhone from "./InputPhone";
+import { motion } from "motion/react";
+import { useForm } from "react-hook-form";
+import { data } from "react-router-dom";
+
+const ContactForm = () => {
+  const inputRef = useRef(null);
+  // const [isChecked, setIsChecked] = useState(false);
+  const [contactData, setContactData] = useState({
+    from_callingCodes: "",
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    } = useForm({
+    defaultValues: {
+      from_full_name: "",
+      from_email: "",
+      from_callingCodes: "",
+      from_phone: "",
+      from_message: "",
+      from_privacy: false,
+    },
+  });
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   //phone number validation spaces, and dashes delete
+  //   const rawPhoneNumber = contactData.from_phone.replace(/[\s-]/g, "");
+  //   const phoneRegex = /^\d{9}$/;
+  //   const phoneValidation = phoneRegex.test(rawPhoneNumber);
+
+  //   await emailjs.send(
+  //     process.env.REACT_APP_EMAILJS_SERVICE_ID,
+  //     process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+  //     {
+  //       ...contactData,
+  //       from_phone: rawPhoneNumber,
+  //     },
+  //     process.env.REACT_APP_EMAILJS_PUBLIC_KEY,
+  //   );
+
+  //   toast.success("Dziękujemy. Wiadomość została wysłana!", {
+  //     position: "top-center",
+  //     className: "toast-success",
+  //   });
+
+  //   setContactData({
+  //     from_full_name: "",
+  //     from_email: "",
+  //     from_phone: "",
+  //     from_message: "",
+  //   });
+  //   setIsChecked(false);
+  // };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const updateField = reusableUpdateField(setContactData);
+
+  return (
+    <motion.form
+      initial={{ opacity: 0, scale: 0.99 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true, amount: 0.2 }}
+      transition={{
+        duration: 0.6,
+        ease: "easeOut",
+      }}
+      className="form-contact-us-main flex-col box-shadow"
+      // onSubmit={handleSubmit}
+
+
+      onSubmit={handleSubmit(
+        (data) => {
+          console.log(data);
+        },
+        (errors) => {
+          toast.error("Uzupełnij wymagane pola", {
+            position: "top-center",
+            className: "toast-success",
+          });
+        },
+      )}
+
+      
+      noValidate
+    >
+      <div className="form-header flex-col">
+        <ion-icon name="paper-plane-outline"></ion-icon>
+        <p className="form-title">Napisz do nas</p>
+        <p className="form-subtitle">Odpowiemy najszybciej jak to możliwe.</p>
+      </div>
+      <div className="flex-row form-ion-row">
+        <ion-icon name="person-outline"></ion-icon>
+        <div className="form-group">
+          <label htmlFor="name">Imię i nazwisko</label>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Wpisz imię i nazwisko"
+            id="name"
+            name="from_full_name"
+            autoComplete="name"
+            // value={contactData.from_full_name}
+            // onChange={(e) => updateField("from_full_name", e.target.value)}
+            {...register("from_full_name", {
+              required: "To pole jest wymagane",
+              minLength: 1,
+            })}
+            className={errors.from_full_name?.message && "error-border"}
+          />
+          <p className="form-error-message">{errors.from_full_name?.message}</p>
+        </div>
+      </div>
+      <div className="flex-row form-ion-row ">
+        <ion-icon name="mail-outline"></ion-icon>
+        <div className="form-group">
+          <label htmlFor="email">Adres e-mail</label>
+          <input
+            type="email"
+            placeholder="Wpisz adres e-mail"
+            id="email"
+            name="from_email"
+            autoComplete="email"
+            // value={contactData.from_email}
+            // onChange={(e) => updateField("from_email", e.target.value)}
+            {...register("from_email", {
+              required: "To pole jest wymagane",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Nieprawidłowy adres e-mail",
+              },
+            })}
+            className={errors.from_email?.message && "error-border"}
+          />
+          <p className="form-error-message">{errors.from_email?.message}</p>
+        </div>
+      </div>
+      {/* ---------- InputPhone ---------- */}
+      <InputPhone
+        contactData={contactData}
+        updateField={updateField}
+        register={register}
+        errors={errors}
+        setValue={setValue}
+        data={data}
+      />
+      <div className="flex-row form-ion-row">
+        <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+        <div className="form-group">
+          <label htmlFor="message">Wiadomość</label>
+          <textarea
+            id="message"
+            placeholder="Napisz, w czym możemy Ci pomóc ..."
+            name="from_message"
+            // value={contactData.from_message}
+            // onChange={(e) => updateField("from_message", e.target.value)}
+            {...register("from_message", {
+              required: "To pole jest wymagane",
+            })}
+          ></textarea>
+          <p className="form-error-message">{errors.from_message?.message}</p>
+        </div>
+      </div>
+      <div className="form-checkbox flex-row">
+        <input
+          type="checkbox"
+          id="privacy"
+          name="privacy"
+          // checked={isChecked}
+          // onChange={(e) => setIsChecked(e.target.checked)}
+          {...register("from_privacy", {
+            required: "To pole jest wymagane",
+          })}
+        />
+
+        <label htmlFor="privacy">
+          Zapoznałem(-am) się z &nbsp;
+          <a href="/polityka-prywatnosci">Polityką Prywatności</a>
+          &nbsp; i wyrażam zgodę na przetwarzanie moich danych osobowych w celu
+          udzielenia odpowiedzi na przesłane zapytanie.
+        </label>
+      </div>{" "}
+      <p className="form-error-message">{errors.from_privacy?.message}</p>
+      <button className="form-button" type="submit">
+        <ion-icon name="paper-plane-outline"></ion-icon>
+        Wyślij wiadomość
+      </button>
+    </motion.form>
+  );
+};
+
+export default ContactForm;
--- src/components/ContactScreen/InputPhone.js
+++ src/components/ContactScreen/InputPhone.js
@@ -1,30 +1,10 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/high-res.css";
 
-const InputPhone = ({ contactData, updateField }) => {
+const InputPhone = ({ contactData, updateField, register, errors, setValue }) => {
   const [localization, setLocalization] = useState(null);
-
-  // const [countries, setCountries] = useState([]);
-
-  // //COUNTRIES.DEV
-  // useEffect(() => {
-  //   const fetchCountries = async () => {
-  //     const response = await axios.get("https://countries.dev/countries");
-  //     console.log(response);
-  //     const filteredByRegion = response.data.filter((country) =>
-  //       country.region.includes("Europe"),
-  //     );
-  //     setCountries(filteredByRegion);
-  //   };
-
-  //   fetchCountries();
-  // }, []);
-
-  // // console.log(countries);
 
   // user localization
   useEffect(() => {
@@ -36,9 +16,11 @@ const InputPhone = ({ contactData, updateField }) => {
         responseLocalization.data.country_calling_code;
       setLocalization(detectedCountry);
       updateField("from_callingCodes", detectedCallingCode);
+     
     };
     fetchLocalization();
-  }, []);
+
+    }, []);
 
   console.log(localization);
 
@@ -48,7 +30,6 @@ const InputPhone = ({ contactData, updateField }) => {
         <ion-icon name="call-outline"></ion-icon>
         <div className="form-group">
           <label htmlFor="phone">Numer telefonu</label>
-
           {/* React-Phone-Input-2 (biblioteka)*/}
           <div className="flex-row">
             <PhoneInput
@@ -60,17 +41,24 @@ const InputPhone = ({ contactData, updateField }) => {
               onChange={(phone) => updateField("from_callingCodes", phone)}
               placeholder=""
             />
-
             <input
               type="tel"
               placeholder="Wpisz numer telefonu"
               id="phone"
               name="from_phone"
               autoComplete="tel"
-              value={contactData.from_phone}
-              onChange={(e) => updateField("from_phone", e.target.value)}
+              // value={contactData.from_phone}
+              // onChange={(e) => updateField("from_phone", e.target.value)}
+              {...register("from_phone", {
+                required: "To pole jest wymagane",
+                pattern: {
+                  value: /^\d{9}$/,
+                  message: "Numer telefonu powinien zawierać 9 cyfr",
+                },
+              })}
             />
           </div>
+          <p className="form-error-message">{errors.from_phone?.message}</p>
         </div>
       </div>
     </>
